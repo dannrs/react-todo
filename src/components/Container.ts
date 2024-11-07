bcda04d507aafcd351664ab191f8d13b5ce44369
dannrs <danniramdhani@proton.me>
2024-11-07 09:06:21 +0700

    styled-components setup

---
+++ src/components/Container.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Container;
---
+++ src/styles/Global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: Inter, sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+`;
+
+export default GlobalStyles;
